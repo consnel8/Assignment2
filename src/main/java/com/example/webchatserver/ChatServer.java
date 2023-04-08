7a9f998b15598114e519e08115ff9c57f494279e
consnel8 <91153372+consnel8@users.noreply.github.com>
2023-04-08 17:08:00 -0400

    commit 2

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -1,13 +1,16 @@
 package com.example.webchatserver;
 
-
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import org.json.JSONObject;
 
 import java.io.IOException;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.SimpleTimeZone;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * This class represents a web socket server, a new connection is created and it receives a roomID as a parameter
@@ -18,7 +21,7 @@ public class ChatServer {
     // contains a static List of ChatRoom used to control the existing rooms and their users
 
     // you may add other attributes as you see fit
-
+    private static final ConcurrentHashMap<String, ChatRoom> chatRooms = new ConcurrentHashMap<>();
 
 
     @OnOpen
@@ -27,14 +30,29 @@ public class ChatServer {
         session.getBasicRemote().sendText("First sample message to the client");
 //        accessing the roomID parameter
         System.out.println(roomID);
-
-
-
+        if (!chatRooms.containsKey(roomID)) {
+            chatRooms.put(roomID, new ChatRoom(roomID, session.getId()));
+            System.out.println("Created new room " + roomID);
+        } else {
+            // add the user to the existing room
+            ChatRoom room = chatRooms.get(roomID);
+            room.setUserName(session.getId(), "");
+            System.out.println("Joined existing room " + roomID);
+        }
     }
 
     @OnClose
     public void close(Session session) throws IOException, EncodeException {
         String userId = session.getId();
+
+        for (ChatRoom roomID : chatRooms.values()) {
+            if (roomID.inRoom(userId)) {
+                roomID.removeUser(userId);
+                System.out.println("The connection to " + roomID + " has closed");
+                System.out.println(userId + " has left room " + roomID.getCode());
+                break;
+            }
+        }
         // do things for when the connection closes
     }
 
@@ -43,15 +61,37 @@ public class ChatServer {
 //        example getting unique userID that sent this message
         String userId = session.getId();
 
-//        Example conversion of json messages from the client
-        //        JSONObject jsonmsg = new JSONObject(comm);
-//        String val1 = (String) jsonmsg.get("attribute1");
-//        String val2 = (String) jsonmsg.get("attribute2");
+        ChatRoom roomID = chatRooms.get(session.getPathParameters().get("roomID"));
 
-        // handle the messages
+        if (roomID == null) {
+            session.getBasicRemote().sendText("Chat room does not exist");
+            session.close();
+            return;
+        }
 
-
+        if (roomID.getUsers().get(userId).equals("")){
+            roomID.setUserName(userId, comm);
+            session.getBasicRemote().sendText("You have joined the chat " + roomID.getCode());
+            broadcastmessage("User " + comm + " has joined the chat" + roomID, userId);
+            return;
+        }
     }
-
-
+    private void broadcastmessage(String comm, ChatRoom roomID, String userIDToExclude) throws IOException {
+        for (String userId : roomID.getUsers().keySet()) {
+            if (!userId.equals(userIDToExclude)){
+                Session userSession = getSessionByID(userId);
+                if (userSession.isOpen()){
+                    userSession.getBasicRemote().sendText(comm);
+                }
+            }
+        }
+    }
+    private Session getSessionByID(String userId) {
+        for (Session session : chatRooms.get(session.getPathParameters().get("roomID")).getUsers().keySet()) {
+            if (session.getId().equals(userId)) {
+                return session;
+            }
+        }
+        return null;
+    }
 }
